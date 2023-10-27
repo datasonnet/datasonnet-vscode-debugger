e75dcee13c43cf42e747b0edbb1a96852845e2a9
Ramiro González Maciel <ramirogm@gmail.com>
2023-10-27 07:52:08 -0300

    test

--- src/ui-test/variables.ts
+++ src/ui-test/variables.ts
@@ -1,9 +1,9 @@
 import * as path from 'path';
 
-// export const TEST_RESOURCES_DIR = path.resolve('.', 'test-resources');
-// export const EXTENSION_DIR = path.join(TEST_RESOURCES_DIR, 'test-extensions');
-// export const WORKBENCH_DIR = path.join(TEST_RESOURCES_DIR, 'ui-workbench');
-// export const RESOURCES_DIR = path.resolve('.', 'src', 'ui-test', 'resources');
+export const TEST_RESOURCES_DIR = path.resolve('.', 'test-resources');
+export const EXTENSION_DIR = path.join(TEST_RESOURCES_DIR, 'test-extensions');
+export const WORKBENCH_DIR = path.join(TEST_RESOURCES_DIR, 'ui-workbench');
+export const RESOURCES_DIR = path.resolve('.', 'src', 'ui-test', 'resources');
 
 // export const DEFAULT_HEADER = 'YamlHeader';
 // export const DEFAULT_PROPERTY = 'yaml-dsl';
